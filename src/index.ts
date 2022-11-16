0b5021721582570090147e3ca212d22c92cc6a96
Uygun Bodur <uygun@dop.com.tr>
2022-11-16 13:30:03 +0300

    fix: socket

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,8 @@ export * as payment from './@domains/payment/payment.model';
 export * as courier from './@domains/courier/q.model';
 
 
+export * as socket_order from './@domains/socket/order.model';
+export * as socket_location from './@domains/socket/location.model';
+export * as socket_constants from './@domains/socket/constants';
+export * as socket_dummy from './@domains/socket/dummy.model';
+export * as socket_admin from './@domains/socket/adminsync.model';
