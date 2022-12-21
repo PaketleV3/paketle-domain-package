e95663503aec7e2de5067c393deaf0dd1f4dc284
Uygun Bodur <uygun@dop.com.tr>
2022-12-21 16:00:10 +0300

    fix:sql

--- src/@application/shift/shift.operation.ts
+++ src/@application/shift/shift.operation.ts
@@ -85,13 +85,13 @@ export class ShiftOperation {
     }
 
 
-    if (response.active_work && response.active_work.is_active_today) {
-      response.is_start_shift = response.active_work.is_start_shift;
-      response.is_startable_shift = response.active_work && !response.active_work.is_start_shift && response.active_work.is_active_state;
+    if (response.next_work && response.next_work.is_active_today) {
+      response.is_start_shift = response.next_work.is_start_shift;
+      response.is_startable_shift = response.next_work && !response.next_work.is_start_shift && response.next_work.is_active_state;
       response.show_shift_answer =
-        !response.active_work.is_start_shift &&
-          !response.active_work.is_shift_answer &&
-          !response.active_work.is_active_state
+        !response.next_work.is_start_shift &&
+          !response.next_work.is_shift_answer &&
+          !response.next_work.is_active_state
           ? true
           : false;
     } else {
